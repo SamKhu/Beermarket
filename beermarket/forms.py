1c12567dab2dc5ac567baca171eb7e96666065ad
SamKhu <shamil.khusainov@mail.ru>
2021-12-08 02:26:55 +0300

    add user data validation and error flashing

--- beermarket/forms.py
+++ beermarket/forms.py
@@ -1,8 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Length, DataRequired, Email, EqualTo
+from wtforms.validators import Length, DataRequired, Email, EqualTo, ValidationError
+from beermarket.models import User
 
 class RegisterForm(FlaskForm):
+    def validate_username(self, username_to_check):
+        user=User.query.filter_by(user_name=username_to_check.data).first()
+        if user:
+            raise ValidationError('Пользователь с таким именем уже существует, попробуйте другое')
+
+    def validate_email(self, email_to_check):
+        email=User.query.filter_by(email=email_to_check.data).first()
+        if email:
+            raise ValidationError('Адрес электронной почты уже используется, проверьте адрес почты')
+
+
     username=StringField(label='Имя Пользователя', validators=[Length(min=2, max=30),DataRequired()])
     email=StringField(label='Электронная почта', validators=[Email(), DataRequired()])
     password_1=PasswordField(label='Пароль', validators=[Length(min=6), DataRequired()])
